36dbe1d5ad243ef29d9c1c009d7c2cab493bbae2
FranklinJaya2006 <148520978+FranklinJaya2006@users.noreply.github.com>
2024-09-27 03:28:13 +0800

    Delete tugas directory

--- tugas/piraberurut.dart
+++
@@ -1,9 +0,0 @@
-void tampilkanPiramidaBerurut(List<int> angka) {
-  int i = 0;
-  while (i < angka.length) {
-    String spasi = ' ' * (angka.length - i - 1);
-    String baris = angka.sublist(0, i + 1).join(' ');
-    print('$spasi$baris');
-    i++;
-  }
-}
